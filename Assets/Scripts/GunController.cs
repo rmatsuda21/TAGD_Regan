dd347e3dc1b65d2cf65d4b39247dff47c140b4f4
Reo Matsuda <rmatsuda@tamu.edu>
2020-10-09 22:26:01 -0500

    Initial Commit

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public GameObject gun;
+
+    public bool isParent = true;
+
+    private float lastShot = 0;
+    public float shotCooldown = .1f;
+
+    private bool canShoot = true;
+
+    private bool isDead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isDead && isParent)
+        {
+            canShoot = Time.time - shotCooldown > lastShot;
+
+            if (Input.GetMouseButton(0) && canShoot)
+            {
+                shoot();
+            }
+        }
+        else
+        {
+            if (GetComponent<ParticleSystem>().isStopped)
+            {
+                Destroy(this);
+            }
+        }
+    }
+
+    public void shoot()
+    {
+        lastShot = Time.time;
+
+        GameObject g = Instantiate(gun, transform.position, transform.rotation);
+        g.AddComponent<GunController>();
+        g.GetComponent<GunController>().isParent = false;
+        g.GetComponent<Rigidbody>().velocity = transform.forward * 10f;
+
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        print("Hello");
+        if(collision.gameObject.tag == "Floor")
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+            GetComponent<ParticleSystem>().Play();
+            isDead = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+
+    public float mouseSensitivity = 1.0f;
+
+    public Transform player;
+
+    private float xRotation = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        player.Rotate(Vector3.up, mouseX);
+    }
+}
